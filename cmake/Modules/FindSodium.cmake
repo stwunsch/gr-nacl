b8e76fa26718152464b9dab1058c14e8bc485012
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2015-04-19 18:22:26 +0200

    add new FindModule for cmake with pkgconfig routine

--- cmake/Modules/FindSodium.cmake
+++ cmake/Modules/FindSodium.cmake
@@ -1,29 +1,28 @@
+find_package(PkgConfig)
+pkg_check_modules(PC_SODIUM QUIET libsodium)
+
 find_path(
-    SODIUM_INCLUDE_DIRS
+    SODIUM_INCLUDE_DIR
     NAMES sodium.h
-          crypto_box.h
-          crypto_stream.h
-          crypto_hash.h
-    HINTS /usr/local/include
-    PATHS /usr/local/include/sodium
-          ../libsodium/src/libsodium/include
+    HINTS ${PC_SODIUM_INCLUDEDIR}
+          ${PC_SODIUM_INCLUDE_DIRS}
+    PATH_SUFFIXES sodium
 )
 
 find_library(
-    SODIUM_LIBRARIES
+    SODIUM_LIBRARY
     NAMES libsodium.so
-    HINTS /usr/local/lib
-    PATHS /usr/local/lib/sodium
-          ../libsodium/src/libsodium/.libs
+    HINTS ${PC_LIBXML_LIBDIR}
+          ${PC_LIBXML_LIBRARY_DIRS}
 )
 
-#message(WARNING "SODIUM INCLUDE " ${SODIUM_INCLUDE_DIRS})
-#message(WARNING "SODIUM LIBRARY " ${SODIUM_LIBRARIES})
+#message(WARNING "SODIUM INCLUDE " ${SODIUM_INCLUDE_DIR})
+#message(WARNING "SODIUM LIBRARY " ${SODIUM_LIBRARY})
 
-if(SODIUM_LIBRARIES AND SODIUM_INCLUDE_DIRS)
+if(SODIUM_LIBRARY AND SODIUM_INCLUDE_DIR)
     include(FindPackageHandleStandardArgs)
-    find_package_handle_standard_args(SODIUM DEFAULT_MSG SODIUM_LIBRARIES SODIUM_INCLUDE_DIRS)
-    mark_as_advanced(SODIUM_LIBRARIES SODIUM_INCLUDE_DIRS)
+    find_package_handle_standard_args(SODIUM DEFAULT_MSG SODIUM_LIBRARY SODIUM_INCLUDE_DIR)
+    mark_as_advanced(SODIUM_LIBRARY SODIUM_INCLUDE_DIR)
 else()
     message(FATAL_ERROR "Sodium is required, but was not found.")
 endif()
